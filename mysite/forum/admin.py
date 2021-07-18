ef060c78513b04a290ce73b319b9ad05b70604b6
Максим <mstarakanov@miem.hse.ru>
2021-07-18 17:43:33 +0500

    new user model

--- mysite/forum/admin.py
+++ mysite/forum/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
-from .models import Users, Sex, Messages
+from .models import SiteUser, Sex, Messages
 
 admin.site.register(Sex)
 
 admin.site.register(Messages)
 
 
-class UsersAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'sex_id', 'date_joined')
-    search_fields = ('username', 'email')
+class SiteUserAdmin(admin.ModelAdmin):
+    ordering = ('-start_date',)
+    list_display = ('username', 'sex', 'email', 'start_date')
+    search_fields = ('username', 'email', 'first_name', 'second_name', 'fathers_name')
+    list_filter = ('sex',)
 
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password', 'photo', 'start_date')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('first_name', 'second_name', 'fathers_name', 'sex', 'birth_date', 'about')})
+    )
 
-admin.site.register(Users, UsersAdmin)
+admin.site.register(SiteUser, SiteUserAdmin)
--- mysite/forum/models.py
+++ mysite/forum/models.py
@@ -1,11 +1,64 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# class Users(AbstractUser):
+#     sex = models.ForeignKey('Sex', on_delete=models.PROTECT, null=True)
+#     photo = models.ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d/', null=True)
+#     bio = models.CharField(verbose_name='Био', max_length=100, blank=True)
+#
+#     class Meta:
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = 'Пользователи'
 
 
-class Users(AbstractUser):
+class SiteAccountManager(BaseUserManager):
+    def create_superuser(self, username, email, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser is_staff must be True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser is_superuser must be True.')
+
+        return self.create_user(username, email, password, **other_fields)
+
+    def create_user(self, username, email, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class SiteUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(_('username'), max_length=20, unique=True)
+    first_name = models.CharField(verbose_name='Имя', max_length=150, blank=True)
+    second_name = models.CharField(verbose_name='Фамилия', max_length=150, blank=True)
+    fathers_name = models.CharField(verbose_name='Отчество', max_length=150, blank=True)
+    start_date = models.DateTimeField(verbose_name='Дата регистрации', default=timezone.now)
+    is_staff = models.BooleanField(verbose_name='Модератор', default=False)
+    is_active = models.BooleanField(verbose_name='Активный пользователь', default=False)
     sex = models.ForeignKey('Sex', on_delete=models.PROTECT, null=True)
     photo = models.ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d/', null=True)
-    bio = models.CharField(verbose_name='Био', max_length=100, blank=True)
+    birth_date = models.DateTimeField(verbose_name='Дата рождения', null=True)
+    about = models.TextField(verbose_name='Био', max_length=500, blank=True)
+
+    objects = SiteAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -27,7 +80,7 @@ class Sex(models.Model):
 class Messages(models.Model):
     text = models.TextField(verbose_name='Текст', max_length=150)
     created_at = models.DateTimeField(verbose_name='Дата отправки', auto_now_add=True)
-    user = models.ForeignKey(Users, on_delete=models.PROTECT)
+    user = models.ForeignKey(SiteUser, on_delete=models.PROTECT)
 
     def __str__(self):
         return str(self.pk)
@@ -36,3 +89,6 @@ class Messages(models.Model):
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
         ordering = ['-created_at']
+
+
+
